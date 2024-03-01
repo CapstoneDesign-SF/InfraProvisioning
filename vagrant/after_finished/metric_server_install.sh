a8d362d7112314e27c1a1ea3dbb8651840521bca
Park MinJe <phone7101@naver.com>
2024-03-01 17:54:38 +0900

    [추가] after_finished 하위에 argocd, metric server 설치를 위한 sh 파일 추가

---
+++ vagrant/after_finished/metric_server_install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+raw_git="raw.githubusercontent.com/CapstoneDesign-SF/InfraProvisioning/main/manifest"
+bin_path="/usr/local/bin"
+
+
+# install metrics-server:3.8.3 by helm
+$bin_path/helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
+$bin_path/helm repo update
+
+$bin_path/helm install metrics-server metrics-server/metrics-server \
+    --create-namespace \
+    --namespace=kube-system \
+    -f https://$raw_git/metric_server_override.yaml \
+    --version 3.12.0
